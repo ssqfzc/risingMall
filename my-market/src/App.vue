925c067936e473f608f4bf41c20fa1f949325641
zhoucan <1006258179@qq.com>
2017-06-15 18:28:32 +0800

    init

---
+++ my-market/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+html{
+  font-size: 16px;
+}
+body{
+  margin:0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 2.6rem;
+}
+.el-dialog--tiny{
+  width: 100% !important;
+}
+.router-link-active{
+  background: #20A0FF;
+}
+.el-message-box{
+  width: 80% !important;
+}
+.el-message{
+  top: 3rem !important;
+}
+.el-time-spinner__list{
+  clear: both;
+}
+.el-time-spinner__list > li{
+  float: left;
+}
+/*html {font-size:10px}*/
+@media screen and (min-width:480px) and (max-width:639px) {
+    html {
+        font-size: 15px
+    }
+}
+@media screen and (min-width:640px) and (max-width:719px) {
+    html {
+        font-size: 20px
+    }
+}
+@media screen and (min-width:720px) and (max-width:749px) {
+    html {
+        font-size: 22.5px
+    }
+}
+@media screen and (min-width:750px) and (max-width:799px) {
+    html {
+        font-size: 23.5px
+    }
+}
+@media screen and (min-width:800px) and (max-width:959px) {
+    html {
+        font-size: 25px
+    }
+}
+@media screen and (min-width:960px) and (max-width:1079px) {
+    html {
+        font-size: 30px
+    }
+}
+@media screen and (min-width:1080px) {
+    html {
+        font-size: 32px
+    }
+}
+</style>
---
+++ my-market/src/components/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <div>
+      <news-media></news-media>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleClick: function () {
+      this.$toast('Hello world!')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
